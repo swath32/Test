3376d6ab8fba0dba8a0c02360105919ed97894f0
Swathi <Swathi@10.0.0.15>
2023-10-20 08:39:56 -0400

    Added Comment

--- SelenemBasics/src/com/seleniom/sample/chrome/Frames.java
+++ SelenemBasics/src/com/seleniom/sample/chrome/Frames.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+// This is for testing
 public class Frames {
 
 	public static void main(String[] args) throws InterruptedException {
